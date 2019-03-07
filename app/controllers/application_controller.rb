c4bfc0e3a462bfa55fa2e8003e079111d17a17ad
bweegee <bw33g33@gmail.com>
2019-03-07 15:07:02 -0700

    Controllers defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def render_error
+    render json: { errors: model.errors.full_messages.join(',') }, status: 418
+  end
 end
