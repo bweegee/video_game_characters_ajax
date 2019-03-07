85e7601abe63004872702b6f782b40d60ff21f7e
bweegee <bw33g33@gmail.com>
2019-03-07 16:13:17 -0700

    view updates for Games and Characters

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -2,13 +2,15 @@ var currentGame = {};
 
 $(document).ready( function() {
 	$('.game-item').on('click', function() {
-		currentGame.id = this.dataset.id
+		currentGame.id = this.dataset.id;
+		currentGame.name = this.dataset.name;
 		$.ajax({
 			url: '/games/' + currentGame.id + '/characters',
 			method: 'GET',
 			datatype: 'JSON'
 		}).done( function(characters) {
 			var list = $('#characters');
+			$('#game').text("Characters in " + currentGame.name);
 			list.empty();
 			characters.forEach( function(char) {
 				var li = '<li data-character-id="' + char.id + '">' + char.name + '</li>'
